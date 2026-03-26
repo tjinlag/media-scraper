2c92e5cd9201357b72d08089540c4fcdc48e0bd3
David Nguyen <tjinlag@gmail.com>
2026-03-26 11:43:48 +0700

    client: add tailwindcss & shadcn

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Toaster } from "@/components/ui/sonner";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
-)
+);
